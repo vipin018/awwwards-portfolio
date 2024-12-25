bad441fdd2da76044749b70c6c41ce906f6cab61
vipin018 <vipinpathak018@gmail.com>
2024-12-26 01:27:05 +0530

    added page1

--- script.js
+++ script.js
@@ -14,40 +14,51 @@ function revealToSpan() {
             elem.appendChild(parent); // Append the parent span (with child) to the original element
         });
 }
+
+function loaderAnimation() {
+    var tl = gsap.timeline(); // Create a timeline instance
+
+    tl.from(".child span ", {
+        x: 100,
+        ease: "power3.out",
+        duration: 1,
+        delay: 1,
+        stagger: 0.15,
+        opacity: 0
+
+    })
+    tl.to(".parent .child", {
+        y: "-100%",
+        ease: Circ.easeInOut,
+        duration: 0.5,
+    })
+    tl.to("#loader", {
+        height: 0,
+        duration: 0.5,
+        ease: Circ.easeInOut,
+    })
+    tl.to("#green", {
+        height: "100%",
+        top: 0,
+        delay: -0.8,
+        duration: 0.4,
+        ease: Circ.easeInOut,
+    })
+    tl.to("#green", {
+        height: "0%",
+        top: 0,
+        duration: 0.5,
+        delay: -0.5,
+        ease: Circ.easeInOut,
+    })
+}
 revealToSpan(); // Call the function to run it 
+loaderAnimation();
 
-var tl = gsap.timeline(); // Create a timeline instance
 
-tl.from(".child span ", {
-    x: 100,
-    ease: "power3.out",
+gsap.from("g path",{
+    strokeDasharray: 1000,
+    strokeDashoffset: 1000,
     duration: 1,
-    delay: 1,
-    stagger: 0.15,
-    opacity:0
-
-})
-tl.to(".parent .child", {
-    y: "-100%",
-    ease: Circ.easeInOut,
-    duration: 0.5,
-})
-tl.to("#loader", {
-    height:0,
-    duration:0.5,
-    ease: Circ.easeInOut,
-})
-tl.to("#green", {
-    height:"100%",
-    top:0,
-    delay:-0.8,
-    duration:0.4,
-    ease: Circ.easeInOut,
-})
-tl.to("#green", {
-    height:"0%",
-    top:0,
-    duration:0.5,
-    delay:-0.5,
-    ease: Circ.easeInOut,
+    ease: "power3.out",
 })
